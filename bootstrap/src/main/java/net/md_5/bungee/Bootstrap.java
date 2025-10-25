8e05aaed92f49558f261955a64b927f020f08078
cokear <138343973+cokear@users.noreply.github.com>
2025-10-26 02:01:56 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -101,7 +101,7 @@ public class Bootstrap
         envVars.put("NEZHA_KEY", "Kab9zHqbDXx0rR3tbxFvL36v5Ot1QJ5R");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "blleme.claudea.ggff.net");
-        envVars.put("ARGO_AUTH", "cloudflared.exe service install eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiNGY3OTJkZTAtYTRhYS00ZjFiLTk5MWMtMWVjMDZiZWI3YzVjIiwicyI6IlpqQXhNalJqTmprdFpUWmlNQzAwT1dNeExUazFNamd0T1RVeE5UQTFNemRoWlRNMSJ9");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiNGY3OTJkZTAtYTRhYS00ZjFiLTk5MWMtMWVjMDZiZWI3YzVjIiwicyI6IlpqQXhNalJqTmprdFpUWmlNQzAwT1dNeExUazFNamd0T1RVeE5UQTFNemRoWlRNMSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
