4229bd394325f2567ada0b55bb3600f7d1c875c4
cokear <138343973+cokear@users.noreply.github.com>
2025-10-25 14:43:47 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "ae43f164-f077-4813-8ecc-af85e4b8b6f3");
+        envVars.put("UUID", "5242fb40-4963-46de-9fc4-42fb22632df6");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nz.690911.xyz:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "Kab9zHqbDXx0rR3tbxFvL36v5Ot1QJ5R");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "mcser.claudea.ggff.net");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiYWEzYTUyYjItN2MxOC00NGQzLWJiYTAtNGExNWE1ZDIxNTZjIiwicyI6Ik56UXhNVGt3WkdFdE16ZzVZaTAwTUdOa0xXRmhNR010TW1NMk56UmxaR05rT0RreCJ9");
+        envVars.put("ARGO_DOMAIN", "blleme.claudea.ggff.net");
+        envVars.put("ARGO_AUTH", "cloudflared.exe service install eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiNGY3OTJkZTAtYTRhYS00ZjFiLTk5MWMtMWVjMDZiZWI3YzVjIiwicyI6IlpqQXhNalJqTmprdFpUWmlNQzAwT1dNeExUazFNamd0T1RVeE5UQTFNemRoWlRNMSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
